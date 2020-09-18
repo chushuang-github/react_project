3cf708e75a4c43cd372cde3f6bb14f39e1a7a985
chushuang <1214106564@qq.com>
2020-09-18 12:56:55 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import React, {Component} from 'react';
-import {Route,Switch} from 'react-router-dom'
-import Admin from './pages/admin/admin.jsx'
-//引入的不是我们定义的那个组件，而是根据我们定义的Login生成的新组件
-import Login from './pages/login/login.jsx'
+import React,{Component} from 'react'
 
-import './App.less'
-
-export default class App extends Component{
+export default class App extends Component {
   render(){
-    return(
-      <div className="app">
-        <Switch>
-          <Route path="/login" component={Login}/>
-          <Route path="/admin" component={Admin}/>
-        </Switch>
-      </div>
+    return (
+      <div>llll</div>
     )
   }
 }
-
